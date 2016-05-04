cf5c1581eab865a77dcec7977c5a395a5ba38445
Bruno Bicudo Cortes <bruno.cortes@castecnologia.com.br>
2016-05-04 13:41:52 -0300

    no message

--- app/src/main/java/com/bruno/gpmap/manage/Register.java
+++ app/src/main/java/com/bruno/gpmap/manage/Register.java
@@ -6,26 +6,42 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.bruno.gpmap.R;
 
 public class Register extends AppCompatActivity {
 
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptRegister();
+            }
+        });
+    }
+
+    private void attemptRegister() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        CreateUser createUser = new CreateUser();
+        createUser.createUser(email, password);
+
     }
 
 }
