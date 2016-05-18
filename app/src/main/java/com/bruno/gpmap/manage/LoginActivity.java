9a3b7e0c34816e94efe3a53de8db9ccf0d1e2e5a
brunocortes06 <brunocortes06@gmail.com>
2016-05-18 00:23:10 -0300

    animação da tela de login revisão do layout da tela de login barra de progresso scrollview

--- app/src/main/java/com/bruno/gpmap/manage/LoginActivity.java
+++ app/src/main/java/com/bruno/gpmap/manage/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.bruno.gpmap.manage;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -15,9 +21,11 @@ import com.bruno.gpmap.map.MapsActivity;
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LoginActivity extends AppCompatActivity
-{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Firebase firebase;
 
@@ -25,36 +33,43 @@ public class LoginActivity extends AppCompatActivity
 
     private EditText passwordEditText;
 
-    private Firebase.AuthResultHandler authCallback = new Firebase.AuthResultHandler()
-    {
+    private View mLoginFormView;
+
+    private View mProgressView;
+
+    private Firebase.AuthResultHandler authCallback = new Firebase.AuthResultHandler() {
 
         @Override
-        public void onAuthenticated (AuthData authData)
-        {
+        public void onAuthenticated(AuthData authData) {
+            showProgress(false);
             goMapActivity(authData.getUid());
         }
 
         @Override
-        public void onAuthenticationError (FirebaseError firebaseError)
-        {
+        public void onAuthenticationError(FirebaseError firebaseError) {
             showError();
         }
     };
+    private ImageView rocketImage;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private boolean ret = false;
 
-    private void showError ()
-    {
+    private void showError(){
+        showProgress(false);
         Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
     }
 
-    private void goMapActivity (String uid)
-    {
+    private void goMapActivity(String uid) {
         finish();
         MapsActivity.start(this, uid);
     }
 
     @Override
-    protected void onCreate (Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -64,29 +79,98 @@ public class LoginActivity extends AppCompatActivity
         Firebase.setAndroidContext(this);
         firebase = new Firebase("https://gpmap.firebaseio.com/");
 
-        ImageView rocketImage = (ImageView) findViewById(R.id.idBotao);
+        rocketImage = (ImageView) findViewById(R.id.idBotao);
         rocketImage.setBackgroundResource(R.drawable.anim_but);
 
-        AnimationDrawable rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
-        rocketAnimation.start();
+        rocketImage.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
     }
 
-    public void login (View view)
-    {
+
+    public void login(View view) {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
         firebase.authWithPassword(email, password, authCallback);
     }
 
-    public void register (View view)
-    {
+    public void register(View view) {
         RegisterActivity.start(this);
     }
 
-    public static void start (Context context)
-    {
+    public static void start(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
         context.startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        AnimationDrawable rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
+        rocketAnimation.start();
+
+
+        checkIfAnimationDone(rocketAnimation);
+    }
+
+    private boolean checkIfAnimationDone(AnimationDrawable anim){
+        final AnimationDrawable a = anim;
+        int timeBetweenChecks = 300;
+        Handler h = new Handler();
+        h.postDelayed(new Runnable(){
+            public void run(){
+                if (a.getCurrent() != a.getFrame(a.getNumberOfFrames() - 1)){
+                    checkIfAnimationDone(a);
+                } else{
+                    showProgress(true);
+                    tryLogin();
+                }
+            }
+        }, timeBetweenChecks);
+        return ret;
+    };
+
+    private void tryLogin(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        firebase.authWithPassword(email, password, authCallback);
+    }
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 }
